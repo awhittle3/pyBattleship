d895fe19db94bc98c253bb3dff27c619d2871592
Andrew Whittle <awhittle@ualberta.ca>
2014-05-08 17:06:42 -0600

    Add comments to boards module for readabilty

--- boards.py
+++ boards.py
@@ -1,12 +1,15 @@
+#Makes the enemy board that the player sees
 def makeEnemyBoard(size):
     board = []
+    #Print ~ for unknown ocean spaces
     for x in range(size):
         board.append('~' * size)
     return board
 
+#Makes the player's board with pre-arranged ships
 def makePlayerBoard(size):
-    x = '~'
-    o = 'o'
+    x = '~' #Empty ocean
+    o = 'o' #Ships
     board = [[x,x,x,x,x,x,x,x,x,x],
              [x,o,o,x,x,x,x,x,x,x],
              [x,x,x,x,x,x,x,x,x,x],
@@ -19,19 +22,22 @@ def makePlayerBoard(size):
              [x,x,x,x,x,x,x,x,x,x],]
     return board
     
-
+#Prints two boards
 def printBoards(board1, board2):
     grid = []
     
     size1 = len(board1)
     size2 = len(board2)
+    #Boards should be the same size
     if size1 != size2:
         print("Error: Player and enemy boards not same size")
-        
+    
+    #Create a grid line string
     for x in range(size1 + 1):
         grid.append("+")
     gridLine = " ".join(grid)
     
+    #Print boards with gridlines
     for line in board1:
         print(gridLine)
         print(" " + " ".join(line))
