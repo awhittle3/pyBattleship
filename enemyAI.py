4e40504077ee171474df1cee6429dcbb1ec76a1c
Andrew Whittle <awhittle@ualberta.ca>
2014-05-09 16:54:52 -0600

    Create functions for targeted AI guesses (incomplete)

--- enemyAI.py
+++ enemyAI.py
@@ -36,7 +36,35 @@ def selectTarget():
                 
     return n
         
-        
-
-def targeting(board, target):
+#Target a ship. i is coordinates of initial hit.
+def targeting(board, i):
     pass
+
+#Guess for a direction
+def targetingGuess(i, vector, board):
+    if validDirection(i, vector, board) == True:
+        if board[row][col] == "o":
+            #It's a hit!
+            board[row][col] = "*"
+        elif board[row][col] == "~":
+            #It's a miss
+            board[row][col] = "X"
+        else:
+            i = addVectors(i,vector)
+    return i
+
+#Takes a target and a vector and checks that direction
+def validDirection(i, vector, board):
+    n = addVectors(i, vector)
+    if (row < 0 or row > BOARD_SIZE - 1) or (col < 0 or col > BOARD_SIZE - 1):
+        return False
+    elif board[row][col] == "X":
+        return False
+    else:
+        return True
+    
+def addVectors(i, j):
+    n = []
+    n.append(i[0] + j[0])
+    n.append(i[1] + j[1])
+    return n
