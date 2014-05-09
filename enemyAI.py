63fb8a8d0a384c68573d49f311d24a3b94974a41
Andrew Whittle <awhittle@ualberta.ca>
2014-05-09 14:12:13 -0600

    Create AI guessing function to guess smartly

--- enemyAI.py
+++ enemyAI.py
@@ -1,7 +1,37 @@
 from random import randint
+from boards import BOARD_SIZE
 
 def guessing(board):
-    pass
+    n = selectTarget()
+    if board[n[0]][n[1]] == "~":
+        #Target was a miss
+        board[n[0]][n[1]] = "X"
+        n = [99,99] #Return a null target
+    elif board[n[0]][n[1]] == "o":
+        #A ship was hit
+        board[n[0]][n[1]] = "*"
+    else:
+        n = guessing(board)
+    return n
+
+def selectTarget():
+    n = []
+    n.append(randint(0, BOARD_SIZE - 1))
+    n.append(randint(0, BOARD_SIZE - 1))
+    
+    #Fit random coordinates to smart grid (spaced apart guesses)
+    if (n[0] + n[1]) % 2 != 0:
+        n[0] += 1
+        if n[0] == BOARD_SIZE:
+            n[0] = 0 #Semantic error: May not satisfy smart grid
+            n[1] += 1
+            if n[1] == BOARD_SIZE:
+                n[0] = 0
+                n[1] = 0
+                
+    return n
+        
+        
 
 def targeting(board, target):
     pass
