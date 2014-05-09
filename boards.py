94962655c765402e1a1b18a1968d5f35b44753eb
Andrew Whittle <awhittle@ualberta.ca>
2014-05-08 19:52:57 -0600

    Remove gridlines and add separator

--- boards.py
+++ boards.py
@@ -24,7 +24,6 @@ def makePlayerBoard(size):
     
 #Prints two boards
 def printBoards(board1, board2):
-    grid = []
     
     size1 = len(board1)
     size2 = len(board2)
@@ -32,18 +31,11 @@ def printBoards(board1, board2):
     if size1 != size2:
         print("Error: Player and enemy boards not same size")
     
-    #Create a grid line string
-    for x in range(size1 + 1):
-        grid.append("+")
-    gridLine = " ".join(grid)
-    
-    #Print boards with gridlines
+    #Print boards with separator
     for line in board1:
-        print(gridLine)
-        print(" " + " ".join(line))
-    print(gridLine)
+        print(" ".join(line))
+    
+    print("-" * size1 * 2)
     
     for line in board2:
-        print(gridLine)
-        print(" " + " ".join(line))
-    print(gridLine)
+        print(" ".join(line))
