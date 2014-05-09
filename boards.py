17e6cf436aa03a9dea8a5d56666a45bc16e34089
Andrew Whittle <awhittle@ualberta.ca>
2014-05-09 13:22:11 -0600

    Add more boards

--- boards.py
+++ boards.py
@@ -37,9 +37,32 @@ board3 = [[x,x,x,x,x,x,x,x,x,x],
           [x,x,x,x,x,o,o,o,x,x],
           [x,x,x,x,x,x,x,x,x,x],]
 
+board4 = [[x,x,x,x,x,x,x,x,x,x],
+          [o,x,x,x,x,x,x,x,x,x],
+          [o,x,x,x,x,o,o,o,o,x],
+          [o,x,x,x,x,x,x,x,x,x],
+          [o,x,x,x,o,o,x,x,x,x],
+          [o,x,x,x,x,x,x,x,x,x],
+          [x,x,x,x,x,x,x,x,x,o],
+          [x,x,x,o,x,x,x,x,x,o],
+          [x,x,x,o,x,x,x,x,x,o],
+          [x,x,x,o,x,x,x,x,x,x],]
+
+board5 = [[o,x,x,x,x,x,x,x,x,x],
+          [o,x,x,x,x,x,o,o,o,o],
+          [x,x,x,x,x,x,x,x,x,x],
+          [x,x,x,x,x,x,x,x,x,x],
+          [x,x,x,o,o,o,o,o,x,x],
+          [x,x,x,x,x,x,x,x,x,x],
+          [x,x,x,x,x,x,x,o,x,x],
+          [x,x,x,o,o,o,x,o,x,x],
+          [x,x,x,x,x,x,x,o,x,x],
+          [x,x,x,x,x,x,x,x,x,x],]
+
 #Select two different random numbers
 def selectRand():
-    n = [randint(1,3), randint(1,3)]
+    i = 5 #number of boards available
+    n = [randint(1,i), randint(1,i)]
     if n[0] == n[1]:
         n = selectRand()
     return n
@@ -49,7 +72,9 @@ def selectBoard(n):
     return {
         1 : board1,
         2 : board1,
-        3 : board3
+        3 : board3,
+        4 : board4,
+        5 : board5
         }.get(n, board1)
 
 #Makes the enemy board that the player sees
