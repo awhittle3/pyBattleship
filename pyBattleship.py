f672d140987614c5e4e80114cf28f2f6350be233
Andrew Whittle <awhittle@ualberta.ca>
2014-05-09 10:19:40 -0600

    Add player input evaluation, determines hit/miss

--- pyBattleship.py
+++ pyBattleship.py
@@ -1,26 +1,54 @@
 import boards
 
 def main():
-    playerDead = False
+    MAX_HITS = 17
     enemyDead = False
+    playerDead = False
+    hitsOnEnemy = 0
+    hitsOnPlayer = 0
+    turn = 1
     
     enemyBoard = boards.makeEnemyBoard()
     enemyLocations = boards.setupEnemyBoard()
     playerBoard = boards.makePlayerBoard()
     
     print("----BATTLESHIP----")
-    boards.printBoards(enemyBoard, playerBoard)
-    
+        
     while not playerDead and not enemyDead:
-        row = int(input("Guess row: "))
-        col = int(input("Guess coloumn: "))
+        boards.printBoards(enemyBoard, playerBoard)
+        print("Turn " + str(turn))
+        row = int(input("Guess row: ")) - 1
+        col = int(input("Guess coloumn: ")) - 1
+        
+        #Player choice evaluated
+        if enemyLocations[row][col] == "o":
+            #It's a hit!
+            enemyBoard[row][col] = "*"
+            hitsOnEnemy += 1
+            
+        else:
+            if(row < 0 or row > boards.BOARD_SIZE - 1) or (col < 0 or col > boards.BOARD_SIZE - 1):
+                print("Oops, that's not even in the ocean.")
+            elif(enemyBoard[row][col] == "X" or enemyBoard[row][col] == "*"):
+                print("You guessed that one already.")
+            else:
+                #It's a miss
+                enemyBoard[row][col] = "X"        
+        
+        #Check if either player is dead
+        if hitsOnEnemy == MAX_HITS:
+            enemyDead = True
+        elif hitsOnPlayer == MAX_HITS:
+            playerDead = True
+        
+        turn += 1
         
         #Make true for testing purposes
         enemyDead = True
     
     if enemyDead:
-        print("You win!")
+        print("YOU WIN!")
     else:
-        print("You lose!")
+        print("YOU LOSE!")
 
 main()
