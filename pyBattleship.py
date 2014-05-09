211a5762dcdffa1285238ff4cb548335456e87be
Andrew Whittle <awhittle@ualberta.ca>
2014-05-08 20:05:59 -0600

    Start work on player input, create while loop

--- pyBattleship.py
+++ pyBattleship.py
@@ -2,10 +2,19 @@ import boards
 
 def main():
     BOARD_SIZE = 10
+    alive = True
+    
     enemyBoard = boards.makeEnemyBoard(BOARD_SIZE)
     playerBoard = boards.makePlayerBoard(BOARD_SIZE)
     
-    print("BATTLESHIP")
+    print("----BATTLESHIP----")
     boards.printBoards(enemyBoard, playerBoard)
+    
+    while alive:
+        row = int(input("Guess row: "))
+        col = int(input("Guess coloumn: "))
+        
+        #Make false for testing purposes
+        alive = False
 
 main()
